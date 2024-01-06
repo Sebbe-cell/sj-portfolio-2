5f85fae43e9be391d440c8f1ef4afacdd542e051
Sebastian Johnsson <Sebastian.johnsson@indpro.se>
2024-01-06 18:06:53 +0100

    nodemailer added.

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -2,8 +2,27 @@ import ParticlesContainer from "../../components/ParticlesContainer";
 
 import { motion } from "framer-motion";
 import { fadeIn } from "../../variants";
+import { sendContactForm } from "../../lib/api";
+import { useState } from "react";
+import Toaster from "../../components/Toaster";
 
 const Contact = () => {
+  const [values, setValues] = useState({ name: "", email: "", subject: "" });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const sendMail = (e) => {
+    e.preventDefault();
+    sendContactForm(values);
+  };
+
   return (
     <div className="h-full">
       <motion.div
@@ -14,14 +33,40 @@ const Contact = () => {
         className="text-center flex flex-col justify-center xl:text-left h-full container mx-auto"
       >
         <div>
-          <h1 className="h1">contact <span className="text-accent">me.</span></h1>
+          <h1 className="h1">
+            contact <span className="text-accent">me.</span>
+          </h1>
         </div>
         <form className="gap-y-6 flex flex-col xl:max-w-[600px] sm:w-full items-center z-10">
-          <input type="text" className="input" placeholder="Name" />
-          <input type="email" className="input" placeholder="Email" autoComplete="off" />
-          <textarea type="text" className="textarea" placeholder="Subject" />
+          <input
+            name="name"
+            value={values.name}
+            onChange={handleInputChange}
+            type="text"
+            className="input"
+            placeholder="Name"
+          />
+          <input
+            name="email"
+            value={values.email}
+            onChange={handleInputChange}
+            type="email"
+            className="input"
+            placeholder="Email"
+            autoComplete="off"
+          />
+          <textarea
+            name="subject"
+            onChange={handleInputChange}
+            value={values.subject}
+            type="text"
+            className="textarea"
+            placeholder="Subject"
+          />
           <div className="flex justify-start w-full">
-          <button className="btn-primary">get in <span className="text-accent">touch.</span></button>
+            <button onClick={sendMail} type="submit" className="btn-primary">
+              get in <span className="text-accent">touch.</span>
+            </button>
           </div>
         </form>
       </motion.div>
