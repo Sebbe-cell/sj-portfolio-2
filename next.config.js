37c51488d0a1fd3ba10cfe14cc1a4d1bf0fdb526
Sebastian Johnsson <Sebastian.johnsson@indpro.se>
2024-01-07 21:00:45 +0100

    config-commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+module.exports = {
+    env: {
+      EMAIL: process.env.EMAIL,
+      PASS: process.env.PASS,
+    },
+  };
