dce1e17e3cacd6709c90c6d10d50a81ed7cc1f2f
Sebastian Johnsson <Sebastian.johnsson@indpro.se>
2024-01-08 15:32:36 +0100

    particalesmod

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -76,7 +76,7 @@ const ParticlesContainer = () => {
               enable: true,
               area: 800,
             },
-            value: 80,
+            value: 100,
           },
           opacity: {
             value: 0.5,
